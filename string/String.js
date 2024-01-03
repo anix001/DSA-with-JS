3d2fcfa22258f0cbebf2b7474b5a17a4888dbb6e
anix001 <chaudharyanish305@gmail.com>
2024-01-03 13:02:36 +0545

    Problem solve: String first Occ find

--- string/String.js
+++ string/String.js
@@ -2,9 +2,19 @@
 
 function findStringOcc(heyStack, needle){
     //super soln
-   //   return heyStack.indexOf(needle);
+   //  return heyStack.indexOf(needle);
 
    //doing myself
-}
+   for(let i = 0; i <= heyStack.length - needle.length ; i++){
+    let j;
 
-console.log(findStringOcc("sadbutsad","sad"));
+    for(j=0; j< needle.length ; j++){
+        if(heyStack[i + j] !== needle[j]) break;
+    }
+
+    if(j === needle.length) return i;
+   }
+
+   return -1;
+}
+console.log(findStringOcc("butsad","sad"));
