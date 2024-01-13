e7b51e83c2c9a43271ecc71741497715b4d956ee
anix001 <chaudharyanish305@gmail.com>
2024-01-13 19:29:58 +0545

    Improvement: Optimized soln for bubble sort

--- sorting/bubble-sort/bubbleSort.js
+++ sorting/bubble-sort/bubbleSort.js
@@ -1,18 +1,32 @@
 const bubbleSort = (arr)=>{
-    for(let i =0; i<arr.length; i++){
-        let temp = arr[0];
-        for(let j=0; j<arr.length-i; j++){
-            if(temp === arr[j]){
-                temp = arr[j];
-            }
-            else if(temp > arr[j]){
-                arr[j-1] = arr[j];
-                arr[j] = temp;
-            }else{
-                temp = arr[j];
+    // for(let i =0; i<arr.length; i++){
+    //     let temp = arr[0];
+    //     for(let j=0; j<arr.length-i; j++){
+    //         if(temp === arr[j]){
+    //             temp = arr[j];
+    //         }
+    //         else if(temp > arr[j]){
+    //             arr[j-1] = arr[j];
+    //             arr[j] = temp;
+    //         }else{
+    //             temp = arr[j];
+    //         }
+    //     }
+    // }
+
+    // optimized version
+     
+    let swapped;
+    do{
+        for(let i =0; i < arr.length -1 ; i++){
+            swapped = false;
+            if(arr[i]> arr[i + 1]){ 
+                [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+                swapped = true;
             }
         }
-    }
+    }while(swapped);
+
     return arr;
 }
 
