4ee6ba4c375db2f9b2f5934ac9abf96bf1db65ea
anix001 <chaudharyanish305@gmail.com>
2024-01-14 14:57:22 +0545

    CodeRefactored: Selection sort

--- sorting/selection-sort/selectionSort.js
+++ sorting/selection-sort/selectionSort.js
@@ -1,15 +1,26 @@
 const selectionSort = (arr) => {
-    for(let j=0; j<arr.length-1; j++){
-        let temp = arr[j]; // 7
-        let smallElement = arr[j], smallElementIndex =j;
-        for(let i=j; i<arr.length-1; i++){
-            if(smallElement > arr[i+1]){
-                smallElement = arr[i+1];
-                smallElementIndex = i+1;
-            }
+    // for(let j=0; j<arr.length-1; j++){
+    //     let temp = arr[j]; // 7
+    //     let smallElement = arr[j], smallElementIndex =j;
+    //     for(let i=j; i<arr.length-1; i++){
+    //         if(smallElement > arr[i+1]){
+    //             smallElement = arr[i+1];
+    //             smallElementIndex = i+1;
+    //         }
+    //     }
+    //     arr[smallElementIndex] = temp;
+    //     arr[j] = smallElement;
+    // }
+
+    //optimized version
+    for(let i = 0; i<arr.length -1 ; i++){
+        let minIndex = i;
+        for(let j = i+1; j<arr.length; j++){
+            if(arr[j] < arr[minIndex]){ minIndex = j;}
         }
-        arr[smallElementIndex] = temp;
-        arr[j] = smallElement;
+        if(minIndex !== i){
+         [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
     }
     return arr;
 };
