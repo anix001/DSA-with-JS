d2b28ccace71f34d7abf6ab580b13a9c9f24968e
anix001 <chaudharyanish305@gmail.com>
2024-01-14 15:58:23 +0545

    CodeRefactored: Insertion sort

--- sorting/insertion-sort/insertionSort.js
+++ sorting/insertion-sort/insertionSort.js
@@ -1,15 +1,27 @@
 const insertionSort= (arr)=>{
-    for(let i =0; i<arr.length-1;i++){
-        if(arr[i]> arr[i+1]){
-            [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
-        }
+    // for(let i =0; i<arr.length-1;i++){
+    //     if(arr[i]> arr[i+1]){
+    //         [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+    //     }
 
-        //again check if every element in front is sorted
-        for(let j=0; j< i; j++){
-            if(arr[j]> arr[i]){
-                [arr[j], arr[i]] = [arr[i], arr[j]]
-            }
+    //     //again check if every element in front is sorted
+    //     for(let j=0; j< i; j++){
+    //         if(arr[j]> arr[i]){
+    //             [arr[j], arr[i]] = [arr[i], arr[j]]
+    //         }
+    //     }
+    // }
+
+
+    //optimized version
+    for(let i = 1; i<arr.length; i++){
+        let curEle  = arr[i];
+        let j = i-1;
+        while(j>=0 && arr[j]> curEle){
+            arr[j+1] = arr[j];
+            j--;
         }
+        arr[j+1] = curEle;
     }
 
     return arr;
