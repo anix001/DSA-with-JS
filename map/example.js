eb2ad26af9416f9ca198e00c4d26b444badcf236
anix001 <chaudharyanish305@gmail.com>
2024-01-16 12:27:00 +0545

    ConceptLearn weakmap plus ProblemSolve: sort character by frequency

--- map/example.js
+++ map/example.js
@@ -1,1 +1,20 @@
 //sort character by frequency
+const sortCharByFrequency = (str) => {
+    //count the no of frequency for every character in string
+    const map = new Map();
+    //let set key-value pair where  key is character and value is count of frequency
+    for(let x of str){
+        map.set(x, (map.get(x)  || 0)+ 1)
+    }
+
+    //sort the map descending value
+    const sortedMaps = [...map.keys()].sort((a, b)=>  map.get(b)- map.get(a));
+    //now generate string according to sorted maps
+    let returnStr='';
+    for(let x of sortedMaps){
+        returnStr += x.repeat(map.get(x));
+    }
+    return returnStr;
+}
+
+console.log(sortCharByFrequency('tree'))
