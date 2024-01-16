8650926c037b940940565c517ef6d18d392fd830
anix001 <chaudharyanish305@gmail.com>
2024-01-16 17:38:31 +0545

    Improvement: added break statement

--- set/question.js
+++ set/question.js
@@ -9,7 +9,10 @@ const intersectionArray = (arr1, arr2) => {
     
    for(let i =0 ; i< arr1.length; i++){
     for(let j =0 ; j<arr2.length; j++){
-        if(arr1[i] === arr2[j] && !arr.includes(arr1[i])) arr.push(arr1[i]);
+        if(arr1[i] === arr2[j] && !arr.includes(arr1[i])) {
+            arr.push(arr1[i]);
+            break;
+        }
     }
    }
 
